c5244f280809cd47f6c6562cb4e05e4fb0a0f80c
Neven <58472164+neven14@users.noreply.github.com>
2021-06-12 12:42:54 +0200

    Add files via upload

---
+++ zavrsnoPonavljanjeZad6/zavrsnoPonavljanjeZad6/KlasaC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanjeZad6
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] Broj)
+        {
+            int Zbroj = Broj.Sum();
+            return Zbroj;
+        }
+        public double Prosjek(int[] Broj)
+        {
+            double Prosjek = Broj.Average();
+            return Prosjek;
+        }
+    }
+}
---
+++ zavrsnoPonavljanjeZad6/zavrsnoPonavljanjeZad6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanjeZad6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zbroj;
+            double prosjek;
+            int[] Brojevi = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Upisite broj " + (i + 1) + " : ");
+                Brojevi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            KlasaC Objekt = new KlasaC();
+            zbroj = Objekt.Zbroj(Brojevi);
+            prosjek = Objekt.Prosjek(Brojevi);
+            Console.WriteLine("Zbroj brojeva je : " + zbroj + " a prosjek brojeva je : " + prosjek);
+            Console.ReadKey();
+        }
+    }
+}
